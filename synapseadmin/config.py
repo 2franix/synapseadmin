ad1ac5af8e0f3788b2afa9eac3d04749f869eda1
Cyrille Defranoux <cyrille.github@youplaboum.fr>
2019-11-03 19:35:55 +0100

    Fix tests

--- synapseadmin/config.py
+++ synapseadmin/config.py
@@ -6,16 +6,19 @@ class Config(object):
     def __init__(self, config_file):
         config_json = None
         with open(config_file, 'r') as f:
-            config_json = json.loads(config_file.read())
+            config_json = json.loads(f.read())
         self._synapse_admin_access_token = config_json['synapse']['accessToken']
         self._db_user = config_json['database']['user']
         self._db_passwd = config_json['database']['password']
 
+    @property
     def synapse_access_token(self):
-        return self._synapse_access_token
+        return self._synapse_admin_access_token
 
+    @property
     def database_user(self):
         return self._db_user
     
+    @property
     def database_passwd(self):
         return self._db_passwd
--- tests/test_config.py
+++ tests/test_config.py
@@ -1,22 +1,28 @@
+import os
 import pytest
 from synapseadmin import config
 
 @pytest.fixture
-def sample_config_file(tmp_path):
-    with open(tmp_path, 'w') as f:
-        f.write("""{
-            'synapse': {
-                'accessToken' : 'access_token_here!'
+def sample_config_file(tmpdir):
+    print('sample_config_file')
+    config_file = os.path.join(tmpdir,'config')
+    with open(config_file, 'w') as f:
+        f.write('''{
+            "synapse": {
+                "accessToken" : "access_token_here!"
             },
-            'database': {
-                'user': 'synapse_user',
-                'password': 'synapse_password_here!'
+            "database": {
+                "user": "synapse_user",
+                "password": "synapse_password_here!"
             }
-        }""")
-    yield tmp_path
+        }''')
+
+    print(f'yield {config_file}')
+    yield config_file
 
 def test_valid_config(sample_config_file):
-   c = config.Config(sample_config_file) 
-   assert c.synapse_access_token == 'access_token_here!' 
-   assert c.database_user == 'synapse_user' 
-   assert c.database_passwd == 'synapse_password_here!' 
+    print(f'test_valid_config')
+    c = config.Config(sample_config_file) 
+    assert c.synapse_access_token == 'access_token_here!' 
+    assert c.database_user == 'synapse_user' 
+    assert c.database_passwd == 'synapse_password_here!' 
